af4720b60049c5138a87cdd36ef71e7c7318358d
Melchor Dominguez <melchor.dominguez@myaureax.com>
2021-08-02 17:30:10 -0400

    - add login page without an available token

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './App.css';
@@ -6,6 +6,12 @@ import './App.css';
 import { Dashboard, Preferences } from './pages';
 
 function App() {
+  const [token, setToken] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
+
   return (
     <div className='wrapper'>
       <h1>Application</h1>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,3 @@
 export { default as Dashboard } from './dashboard';
+export { default as Login } from './login';
 export { default as Preferences } from './preferences';
