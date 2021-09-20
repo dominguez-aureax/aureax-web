32b46e38f0c8001ee59214feea64829cda67874a
Melchor Dominguez <melchor.dominguez@myaureax.com>
2021-09-20 14:02:23 -0400

    - #5: style active

--- src/components/sidebar/component.js
+++ src/components/sidebar/component.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 import React, { useState } from 'react';
+import { useLocation } from 'react-router';
 import { Collapse, Container, Row, Nav, Navbar } from 'react-bootstrap';
 import { MdPeople, MdPersonAdd, MdLibraryAdd, MdStarBorder, MdPieChart } from 'react-icons/md';
 
@@ -8,6 +9,12 @@ import './index.scss';
 const Component = () => {
   const [collapseOpen, setCollapseOpen] = useState();
   const logo = <img className='logo' src='./favicon.ico' />;
+  const location = useLocation();
+  console.log(`Sidebar --- ${location.pathname}`);
+
+  const activeRoute = (routeName) => {
+    return location.pathname.indexOf(routeName) > -1 ? true : false;
+  };
 
   // toggles collapse between opened and closed (true/false)
   const toggleCollapse = () => {
@@ -22,23 +29,23 @@ const Component = () => {
     <Navbar className='sidebar' expand='m' id='sidebar-main'>
       <Row className='company-row'>{logo}</Row>
       <div className='line' />
-      <Nav.Link href='/dashboard'>
+      <Nav.Link href='/dashboard' active={activeRoute('/dashboard')}>
         <MdPieChart size={25} />
         Dashboard
       </Nav.Link>
-      <Nav.Link href='/members'>
+      <Nav.Link href='/members' active={activeRoute('/members')}>
         <MdPeople size={25} />
         Members
       </Nav.Link>
-      <Nav.Link href='/referrals'>
+      <Nav.Link href='/referrals' active={activeRoute('/referrals')}>
         <MdPersonAdd size={25} />
         Referrals
       </Nav.Link>
-      <Nav.Link href='/jobs'>
+      <Nav.Link href='/jobs' active={activeRoute('/jobs')}>
         <MdLibraryAdd size={25} />
         Jobs
       </Nav.Link>
-      <Nav.Link href='/campaign'>
+      <Nav.Link href='/campaign' active={activeRoute('/campaign')}>
         <MdStarBorder size={25} />
         Campaign
       </Nav.Link>
